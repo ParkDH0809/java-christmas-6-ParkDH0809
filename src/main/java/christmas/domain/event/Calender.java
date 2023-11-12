9511bf860194a0191c89336fee698815a5cfb7ac
ParkDH0809 <znxn1237@gmail.com>
2023-11-13 00:42:45 +0900

    feat: 이벤트 달력 구현

---
+++ src/main/java/christmas/domain/event/Calender.java
@@ -0,0 +1,21 @@
+package christmas.domain.event;
+
+import java.util.List;
+
+public enum Calender {
+    STAR_DATE(List.of(3,10,17,24,35,31)),
+    WEEKEND_DATE(List.of(1, 2, 8, 9, 15, 16, 22, 23, 29, 30));
+
+    private List<Integer> eventDates;
+    Calender(List<Integer> eventDates) {
+        this.eventDates = eventDates;
+    }
+
+    static boolean isStarDate(int date) {
+        return STAR_DATE.eventDates.stream().anyMatch(event -> event.equals(date));
+    }
+
+    static boolean isWeekendDate(int date) {
+        return WEEKEND_DATE.eventDates.stream().anyMatch(event -> event.equals(date));
+    }
+}
