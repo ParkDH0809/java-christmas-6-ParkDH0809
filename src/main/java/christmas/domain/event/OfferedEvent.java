8939764856e2face4f160506b8b41eacfa5291a5
ParkDH0809 <znxn1237@gmail.com>
2023-11-13 00:18:29 +0900

    feat: OfferedEvent 구현

---
+++ src/main/java/christmas/domain/event/OfferedEvent.java
@@ -0,0 +1,22 @@
+package christmas.domain.event;
+
+public class OfferedEvent {
+    private final String eventName;
+    private final int discountAmount;
+    private OfferedEvent(String eventName, int discountAmount) {
+        this.eventName = eventName;
+        this.discountAmount = discountAmount;
+    }
+
+    public static OfferedEvent of(String eventName, int discountAmount) {
+        return new OfferedEvent(eventName, discountAmount);
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public int getDiscountAmount() {
+        return discountAmount;
+    }
+}
