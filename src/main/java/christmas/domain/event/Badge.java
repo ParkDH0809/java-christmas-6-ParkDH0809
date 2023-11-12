a4188cb263aaa7065bcef1819fb58938e28f0e52
ParkDH0809 <znxn1237@gmail.com>
2023-11-13 01:24:19 +0900

    feat: Badge 클래스 구현

---
+++ src/main/java/christmas/domain/event/Badge.java
@@ -0,0 +1,29 @@
+package christmas.domain.event;
+
+public enum Badge {
+    SANTA(20000, "산타"),
+    TREE(10000, "트리"),
+    STAR(5000, "별"),
+    NONE(0, "없음");
+
+    private int amount;
+    private String name;
+
+    Badge(int amount, String name) {
+        this.amount = amount;
+        this.name = name;
+    }
+
+    static String getBadge(int amount) {
+        if(amount >= SANTA.amount) {
+            return SANTA.name;
+        }
+        if(amount >= TREE.amount) {
+            return TREE.name;
+        }
+        if (amount >= STAR.amount) {
+            return STAR.name;
+        }
+        return NONE.name;
+    }
+}
