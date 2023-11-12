efbc2dd0f151610f9beaca63ee10ef4e16ec5205
ParkDH0809 <znxn1237@gmail.com>
2023-11-13 00:15:33 +0900

    feat: 혜택 내역 출력 포맷 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -7,6 +7,8 @@ public class OutputView {
     private static final String OUTPUT_BEFORE_DISCOUNT = "\n<할인 전 총주문 금액>\n%,d원\n";
     private static final String OUTPUT_PRESENT_EVENT_TITLE = "\n<증정 메뉴>";
     private static final String OUTPUT_NONE = "없음";
+    private static final String OUTPUT_BENEFIT_TITLE = "\n<혜택 내역>";
+    private static final String OUTPUT_BENEFIT = "%s: -%,d원\n";
 
     public void outputEventPreviewPhrase(int number) {
         System.out.printf(OUTPUT_EVENT_PREVIEW_PHRASE, number);
@@ -30,5 +32,13 @@ public class OutputView {
 
     public void outputNone() {
         System.out.println(OUTPUT_NONE);
+    }
+
+    public void outputBenefitTitle() {
+        System.out.println(OUTPUT_BENEFIT_TITLE);
+    }
+
+    public void outputBenefit(String string, int number) {
+        System.out.printf(OUTPUT_BENEFIT, string, number);
     }
 }
