df8babebec3815504b297ba0eee61ada591207e8
ParkDH0809 <znxn1237@gmail.com>
2023-11-13 15:33:44 +0900

    refactor: ChristmasDDayEvent 클래스 리팩토링

--- src/main/java/christmas/domain/event/ChristmasDDayEvent.java
+++ src/main/java/christmas/domain/event/ChristmasDDayEvent.java
@@ -1,9 +1,19 @@
 package christmas.domain.event;
 
 public class ChristmasDDayEvent {
+
+    private static final String CHRISTMAS_D_DAY_EVENT_NAME = "크리스마스 디데이 할인";
     private static final int D_DAY = 25;
+    private static final int BASIC_DISCOUNT_AMOUNT = 1000;
+    private static final int ADDITIONAL_DISCOUNT_AMOUNT = 100;
+
+
     OfferedEvent getDDayEventBenefit(int visitDate) {
-        return OfferedEvent.of("크리스마스 디데이 할인", 1000 + ((visitDate - 1) * 100));
+        return OfferedEvent.of(CHRISTMAS_D_DAY_EVENT_NAME, getDiscountAmount(visitDate));
+    }
+
+    private int getDiscountAmount(int visitDate) {
+        return BASIC_DISCOUNT_AMOUNT + (ADDITIONAL_DISCOUNT_AMOUNT * (visitDate - 1));
     }
 
     boolean isWithinPeriod(int visitDate) {
