4b0e6282add07d53f32c51c91832bc6d3c5d9312
ParkDH0809 <znxn1237@gmail.com>
2023-11-13 15:40:48 +0900

    refactor: SpecialEvent 클래스 리팩토링

--- src/main/java/christmas/domain/event/SpecialEvent.java
+++ src/main/java/christmas/domain/event/SpecialEvent.java
@@ -2,7 +2,10 @@ package christmas.domain.event;
 
 public class SpecialEvent {
 
+    private static final String SPECIAL_EVENT_NAME = "특별 할인";
+    private static final int DISCOUNT_AMOUNT = 1000;
+
     OfferedEvent getSpecialEventBenefit() {
-        return OfferedEvent.of("특별 할인", 1000);
+        return OfferedEvent.of(SPECIAL_EVENT_NAME, DISCOUNT_AMOUNT);
     }
 }
