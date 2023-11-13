95e2651345e7b12ab82d2d92036ebc58bd4d0b67
ParkDH0809 <znxn1237@gmail.com>
2023-11-13 15:41:57 +0900

    refactor: WeekdayEvent 클래스 리팩토링

--- src/main/java/christmas/domain/event/WeekdayEvent.java
+++ src/main/java/christmas/domain/event/WeekdayEvent.java
@@ -1,7 +1,12 @@
 package christmas.domain.event;
 
 public class WeekdayEvent {
+
+    private static final String WEEKDAY_EVENT_NAME = "평일 할인";
+    private static final int DISCOUNT_AMOUNT = 2023;
+
     private final int numberOfDesertMenu;
+
     private WeekdayEvent(int numberOfDesertMenu) {
         this.numberOfDesertMenu = numberOfDesertMenu;
     }
@@ -11,6 +16,6 @@ public class WeekdayEvent {
     }
 
     OfferedEvent getWeekdayEventBenefit() {
-        return OfferedEvent.of("평일 할인", numberOfDesertMenu * 2023);
+        return OfferedEvent.of(WEEKDAY_EVENT_NAME, numberOfDesertMenu * DISCOUNT_AMOUNT);
     }
 }
