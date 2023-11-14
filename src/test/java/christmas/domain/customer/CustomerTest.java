60894a75ee2f0d31c1802db30b6c9cb3bf265460
ParkDH0809 <znxn1237@gmail.com>
2023-11-14 15:54:16 +0900

    test: 이벤트 적용 대상 판별 테스트 구현

---
+++ src/test/java/christmas/domain/customer/CustomerTest.java
@@ -0,0 +1,25 @@
+package christmas.domain.customer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CustomerTest {
+
+    @Test
+    @DisplayName("손님 - 이벤트 적용 대상이 맞는 경우")
+    void testIsTargetForEvent() {
+        Customer customer = Customer.of(VisitDate.from("3"), OrderMenus.from("타파스-4,제로콜라-1"));
+        assertThat(customer.isTargetForEvent()).isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("손님 - 이벤트 적용 대상이 아닌 경우")
+    void testIsNotTargetForEvent() {
+        Customer customer = Customer.of(VisitDate.from("3"), OrderMenus.from("타파스-1,제로콜라-1"));
+        assertThat(customer.isTargetForEvent()).isEqualTo(false);
+    }
+
+
+}
