e358b6251ab7a9cb6eaec9ead53dc999ff647ead
ParkDH0809 <znxn1237@gmail.com>
2023-11-14 17:16:48 +0900

    test: 크리스마스 디데이 할인 적용 테스트 구현

---
+++ src/test/java/christmas/domain/event/ChristmasDDayEventTest.java
@@ -0,0 +1,36 @@
+package christmas.domain.event;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ChristmasDDayEventTest {
+
+    ChristmasDDayEvent christmasDDayEvent;
+
+    @BeforeEach
+    void initChristmasDDayEvent() {
+        christmasDDayEvent = new ChristmasDDayEvent();
+    }
+
+    @Test
+    @DisplayName("크리스마스 디데이 이벤트 테스트 - 적용이 되는 날짜인 경우")
+    void testIsWithinPeriod() {
+        assertThat(christmasDDayEvent.isWithinPeriod(25)).isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("크리스마스 디데이 이벤트 테스트 - 적용이 되지 않는 날짜인 경우")
+    void testIsNotWithinPeriod() {
+        assertThat(christmasDDayEvent.isWithinPeriod(26)).isEqualTo(false);
+    }
+
+    @Test
+    @DisplayName("크리스마스 디데이 이벤트 테스트 - 정보 확인")
+    void testDDayEventBenefit() {
+        OfferedEvent offeredEvent = OfferedEvent.of("크리스마스 디데이 할인", 3400);
+        assertThat(offeredEvent).usingRecursiveComparison().isEqualTo(christmasDDayEvent.getDDayEventBenefit(25));
+    }
+}
